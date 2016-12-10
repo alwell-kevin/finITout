c26fd23bec82094fe2c247e0f343696649f49d8a
Kevin Alwell <kalwell@LTNJ26251.local>
2016-12-10 11:53:42 -0500

    trans list var naming

--- Frontend/www/js/controllers.js
+++ Frontend/www/js/controllers.js
@@ -46,58 +46,59 @@ angular.module('starter.controllers', [])
   }
 })
 
-.controller('TransactionCtrl', function($scope, $state,$ionicPopup) {
-  $scope.data = {};
-  $scope.transactions = [{ elderID: 234234234, timestamp: "01/27/92", Status: 0, Amount: 9999, Description: "Status is false", Notification: 0, Fraud:0 },
-    { elderID: 234234234, timestamp: "01/27/92", Status: 1, Amount: 9999, Description: "this is approved", Notification: 0, Fraud:0 },
-    { elderID: 234234234, timestamp: "01/27/92", Status: null, Amount: 9999, Description: "Has no status is pending", Notification: 0, Fraud:0 },
-    { elderID: 234234234, timestamp: "01/27/92", Status: true, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud:0 }, { elderID: 234234234, timestamp: "01/27/92", Status: true, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud:0 }, { elderID: 234234234, timestamp: "01/27/92", Status: 0, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud:0 }, { elderID: 234234234, timestamp: "01/27/92", Status: 0, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud:0 }, { elderID: 234234234, timestamp: "01/27/92", Status: true, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud:0 }, { elderID: 234234234, timestamp: "01/27/92", Status: 1, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud:0 }, { elderID: 234234234, timestamp: "01/27/92", Status: 0, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud:0 }, { elderID: 234234234, timestamp: "01/27/92", Status: 0, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud:0 }, { elderID: 234234234, timestamp: "01/27/92", Status: 1, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud:0 }, { elderID: 234234234, timestamp: "01/27/92", Status: 0, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud:0 }, { elderID: 234234234, timestamp: "01/27/92", Status: true, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud:0 }, { elderID: 234234234, timestamp: "01/27/92", Status: 0, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud:0 }];
-  $scope.getItem = function(transaction){
-    $state.go("tab.details", {obj:transaction});
-  }
-})
-  .controller('DetailsCtrl', function($scope,$state,$ionicPopup,$location) {
-      $scope.hideTime = true;
-      $scope.transaction = {};
+.controller('TransactionCtrl', function($scope, $state, $ionicPopup) {
+    $scope.data = {};
+    $scope.transactions = [{ Date: "2016-06-30T04:00:00.000Z", elderID: 234234234, timestamp: "01/27/92", Status: 0, Amount: 9999, Description: "Status is false", Notification: 0, Fraud: 0 },
+      { Date: "2016-06-30T04:00:00.000Z", elderID: 234234234, timestamp: "01/27/92", Status: 1, Amount: 9999, Description: "this is approved", Notification: 0, Fraud: 0 },
+      { Date: "2016-06-30T04:00:00.000Z", elderID: 234234234, timestamp: "01/27/92", Status: null, Amount: 9999, Description: "Has no status is pending", Notification: 0, Fraud: 0 },
+      { elderID: 234234234, timestamp: "01/27/92", Status: true, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud: 0 }, { elderID: 234234234, timestamp: "01/27/92", Status: true, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud: 0 }, { elderID: 234234234, timestamp: "01/27/92", Status: 0, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud: 0 }, { elderID: 234234234, timestamp: "01/27/92", Status: 0, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud: 0 }, { elderID: 234234234, timestamp: "01/27/92", Status: true, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud: 0 }, { elderID: 234234234, timestamp: "01/27/92", Status: 1, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud: 0 }, { elderID: 234234234, timestamp: "01/27/92", Status: 0, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud: 0 }, { elderID: 234234234, timestamp: "01/27/92", Status: 0, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud: 0 }, { elderID: 234234234, timestamp: "01/27/92", Status: 1, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud: 0 }, { elderID: 234234234, timestamp: "01/27/92", Status: 0, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud: 0 }, { elderID: 234234234, timestamp: "01/27/92", Status: true, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud: 0 }, { elderID: 234234234, timestamp: "01/27/92", Status: 0, Amount: 9999, Description: "this is a Description", Notification: 0, Fraud: 0 }
+    ];
+    $scope.getItem = function(transaction) {
+      $state.go("tab.details", { obj: transaction });
+    }
+  })
+  .controller('DetailsCtrl', function($scope, $state, $ionicPopup, $location) {
+    $scope.hideTime = true;
+    $scope.transaction = {};
 
-      console.log($state.params.obj);
-      $scope.transaction = $state.params.obj;
-      $scope.decision = function(value){
-        $scope.userResponse = {};
-        if(value){
-          $ionicPopup.alert({
-            title: 'Transaction Approved',
-            template: '<ion-checkbox ng-model="userResponse.Notification"><small>Do you want to be notified if this reoccurs?</small></ion-checkbox>'
-            scope: $scope
-          }).then(function(res){
-            console.log($scope.userResponse);
-            console.log($scope.userResponse.Notification);
-            userResponse ={
-              Status: 1
-            };
-            $location.path("/tab/transactionlist");
-          });
+    console.log($state.params.obj);
+    $scope.transaction = $state.params.obj;
+    $scope.decision = function(value) {
+      $scope.userResponse = {};
+      if(value) {
+        $ionicPopup.alert({
+          title: 'Transaction Approved',
+          template: '<ion-checkbox ng-model="userResponse.Notification"><small>Notify for vendor?</small></ion-checkbox>',
+          scope: $scope
+        }).then(function(res) {
+          if($scope.userResponse.Notification) {
+            $scope.userResponse.Notification = 1;
+          } else { $scope.userResponse.Notification = 1; }
 
-        }
-        else{
-          $ionicPopup.alert({
-            title: 'Transaction Rejected',
-            template: '<ion-checkbox ng-model="userResponse.Fraud">Mark as Fraud if reoccurs?</ion-checkbox>'
-          }).then(function(res){
+          $scope.userResponse = {
+            Status: 1
+          };
+          $location.path("/tab/transactionlist");
+        });
 
-            console.log($scope.userResponse.Fraud);
-            userResponse ={
-              Status: 0
-            };
-            $location.path("/tab/transactionlist");
-          });
+      } else {
+        $ionicPopup.alert({
+          title: 'Transaction Rejected',
+          template: '<ion-checkbox ng-model="userResponse.Fraud">Notify for vendor?</ion-checkbox>'
+        }).then(function(res) {
 
-        }
+          console.log($scope.userResponse.Fraud);
+          userResponse = {
+            Status: 0
+          };
+          $location.path("/tab/transactionlist");
+        });
 
       }
 
     }
-  )
+
+  })
 
 .controller('NewHomeCtrl', function($scope, $state) {
   $scope.goList = function() {
@@ -137,68 +138,68 @@ angular.module('starter.controllers', [])
 })
 
 .controller('ChartCtrl', function($scope) {
-    console.log("inside ChartCtrl");
-    $scope.vm = {};
-    $scope.vm.options = {};
-    $scope.vm.data = {};
-    $scope.data1 = [];
-    var socket = io.connect("http://192.168.5.38:1337");
-    socket.on("message", function(message) {
-      console.log(message);
-      $scope.data1.push({ x: new Date(), y: message });
-      $scope.$apply();
-    });
+  console.log("inside ChartCtrl");
+  $scope.vm = {};
+  $scope.vm.options = {};
+  $scope.vm.data = {};
+  $scope.data1 = [];
+  var socket = io.connect("http://192.168.5.38:1337");
+  socket.on("message", function(message) {
+    console.log(message);
+    $scope.data1.push({ x: new Date(), y: message });
+    $scope.$apply();
+  });
 
-    $scope.vm.options = {
-      chart: {
-        type: 'lineChart',
-        height: 450,
-        margin: {
-          top: 20,
-          right: 20,
-          bottom: 40,
-          left: 55
-        },
-        x: function(d) {
-          return d.x;
-        },
-        y: function(d) {
-          return d.y;
-        },
-
-        xAxis: {
-          axisLabel: 'Time (seconds)',
-          tickFormat: function(d) {
-            return d3.time.format('%S')(new Date(d));
-          }
-        },
-        yAxis: {
-          axisLabel: 'No. of Tilts',
-          tickFormat: function(d) {
-            return d3.format('.02f')(d);
-          },
-          axisLabelDistance: -10
-        },
-
+  $scope.vm.options = {
+    chart: {
+      type: 'lineChart',
+      height: 450,
+      margin: {
+        top: 20,
+        right: 20,
+        bottom: 40,
+        left: 55
       },
-      title: {
-        text: 'Customer Checkout Peak Hours'
-      }
+      x: function(d) {
+        return d.x;
+      },
+      y: function(d) {
+        return d.y;
+      },
 
-    };
+      xAxis: {
+        axisLabel: 'Time (seconds)',
+        tickFormat: function(d) {
+          return d3.time.format('%S')(new Date(d));
+        }
+      },
+      yAxis: {
+        axisLabel: 'No. of Tilts',
+        tickFormat: function(d) {
+          return d3.format('.02f')(d);
+        },
+        axisLabelDistance: -10
+      },
 
-    $scope.vm.data = [
+    },
+    title: {
+      text: 'Customer Checkout Peak Hours'
+    }
 
-      {
-        values: $scope.data1
+  };
 
-      }
+  $scope.vm.data = [
 
-    ];
-    console.log("ending ChartCtrl");
-  })
+    {
+      values: $scope.data1
+
+    }
+
+  ];
+  console.log("ending ChartCtrl");
+})
 
 
 
-  .controller('ProfileCtrl', function($scope) {})
+.controller('ProfileCtrl', function($scope) {})
   .controller('CallCtrl', function($scope) {});
